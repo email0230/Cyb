527c255e66af3dbd8907df41656365b83bed19a2
Motrixox <69849057+Motrixox@users.noreply.github.com>
2023-11-15 16:19:24 +0100

    image captcha attempts

---
+++ Cyb_mcfr/Models/Captcha.cs
@@ -0,0 +1,20 @@
+﻿namespace Cyb_mcfr.Models
+{
+    public class Captcha
+    {
+        public string FileName { get; set; }
+        public string WhatToFind { get; set; }
+        public int AnswerX { get; set; }
+        public int AnswerY { get; set; }
+
+        public static Captcha GetRandomCaptcha()
+        {
+            var list = new List<Captcha>
+            {
+                new Captcha{ FileName = "owoce.jpg", WhatToFind = "orange", AnswerX = 162, AnswerY = 158 }
+            };
+
+            return list.First();
+        }
+    }
+}
