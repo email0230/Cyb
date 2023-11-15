e6d66656aed0ed6c2d786bcd1f2b43674983a466
Motrixox <69849057+Motrixox@users.noreply.github.com>
2023-11-15 18:03:05 +0100

    new captcha pictures

--- Cyb_mcfr/Models/Captcha.cs
+++ Cyb_mcfr/Models/Captcha.cs
@@ -11,10 +11,17 @@
         {
             var list = new List<Captcha>
             {
-                new Captcha{ FileName = "owoce.jpg", WhatToFind = "orange", AnswerX = 162, AnswerY = 158 }
+                new Captcha{ FileName = "owoce.jpg", WhatToFind = "orange", AnswerX = 162, AnswerY = 158 },
+                new Captcha{ FileName = "owoce2.jpg", WhatToFind = "bananas", AnswerX = 34, AnswerY = 161 },
+                new Captcha{ FileName = "owoce3.jpg", WhatToFind = "apple", AnswerX = 40, AnswerY = 53 },
+                new Captcha{ FileName = "owoce4.jpg", WhatToFind = "strawberry", AnswerX = 106, AnswerY = 49 },
+                new Captcha{ FileName = "owoce5.jpg", WhatToFind = "watermelon", AnswerX = 142, AnswerY = 149 },
+                new Captcha{ FileName = "owoce6.jpg", WhatToFind = "carrot", AnswerX = 37, AnswerY = 103 },
             };
 
-            return list.First();
+            var x = Random.Shared.Next(0, list.Count);
+
+            return list[x];
         }
     }
 }
